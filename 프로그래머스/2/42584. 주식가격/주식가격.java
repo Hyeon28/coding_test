b4b427044454016dd2845f0247fa2aa79c416c55
Hyeon28 <96675421+Hyeon28@users.noreply.github.com>
2024-11-08 14:56:08 +0900

    [level 2] Title: 주식가격, Time: 20.67 ms, Memory: 70.6 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,18 +1,20 @@
 class Solution {
     public int[] solution(int[] prices) {
-        int[] answer = new int[prices.length];  // 결과를 담을 배열
-        
-        for (int i = 0; i < prices.length; i++) {
-            int result = 0;  // 가격이 떨어지지 않은 시간을 기록할 변수
+        int[] answer = new int[prices.length]; 
+
+        for (int i = 0; i < prices.length - 1; i++) {  
+            int result = 0;  
             for (int j = i + 1; j < prices.length; j++) {
-                result++;  // 가격이 떨어지지 않는 기간을 1초씩 증가
-                if (prices[i] > prices[j]) {  // 가격이 떨어지면 그 시점에서 멈춤
+                result++; 
+                if (prices[i] > prices[j]) {  
                     break;
                 }
             }
-            answer[i] = result;  // 해당 시점에 떨어지지 않은 시간 기록
+            answer[i] = result; 
         }
-        
-        return answer;  // 결과 배열 반환
+
+        answer[prices.length - 1] = 0;  
+
+        return answer; 
     }
 }
