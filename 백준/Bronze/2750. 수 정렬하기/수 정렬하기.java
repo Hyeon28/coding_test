b47db0c1253d93e59caaf7adabd59a532f82e231
Hyeon28 <96675421+Hyeon28@users.noreply.github.com>
2025-01-02 19:10:02 +0900

    [Bronze II] Title: 수 정렬하기, Time: 272 ms, Memory: 22424 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -2,7 +2,6 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        //array.sort가 아닌 정렬을 통해서 해결
         Scanner sc = new Scanner(System.in);
         int count = sc.nextInt();
         int nums[] = new int[count];
@@ -11,23 +10,8 @@ public class Main {
             nums[i] = sc.nextInt(); 
         }
 
-        for(int i = 0; i<count-1; i++) {
-            boolean swap = false; //swap이 없는 경우 바로 break
-
-            for(int j = 0; j<count-1-i; j++) {
-                int a = nums[j];
-                int b = nums[j + 1];
-                if (a > b) { //앞 숫자가 더 큰 경우 두 숫자의 위치를 swap
-                    nums[j] = b;
-                    nums[j+1] = a;
-                    swap = true;
-                }
-            }
-            if(!swap) break;
-        }
-
-        for(int i = 0; i<count; i++){
-            System.out.println(nums[i]);
-        }
+        Arrays.sort(nums);
+        for (Integer n : nums) 
+            System.out.println(n);
     }
 }
